0da5c7051e05e8072efcf0e4c7c51bdc46a7e37a
“stevenmunoz” <steven.munoz90@gmail.com>
2024-09-20 18:08:15 -0500

    Add user model

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+//user this model anywhere else in the App
+export default User;
