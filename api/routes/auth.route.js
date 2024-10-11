5ee525f46881b4a0f223efe058f2f7b8a2a57459
“stevenmunoz” <steven.munoz90@gmail.com>
2024-10-10 20:55:59 -0500

    Complete signin functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -5,6 +5,5 @@ const router = express.Router(); // Create a new router
 
 router.post('/signup', signup); // Add a new route to the router
 router.post('/signin', signin); // Add a new route to the router
-router.get('/testauth', testauth); // Add a new route to the router
 
 export default router; // Export the router so it can be used in other files     
