545117465cd99adb4c744e9bdfe1149712d60af0
“stevenmunoz” <steven.munoz90@gmail.com>
2024-10-10 20:35:01 -0500

    Add signin route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+
+export const testauth = (req, res) => {
+    res.json({ message: 'API Route for auth working!' });
+};
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body; // Destructure the username, email and password from req.body
@@ -10,6 +15,26 @@ export const signup = async (req, res, next) => {
     try {
         await newUser.save() // Save the new user
         res.status(201).json({ message: 'User signed up successfully!' }); // If successful, send a message
+    } catch (error) {  
+        next(error); // If there is an error, pass it to the error handling middleware
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body; // Destructure the email and password from req.body
+    try {
+        const validUser = await User.findOne({ email }); // Find the user by email
+        if (!validUser) { // If the user is not found
+            return next(errorHandler(404, 'User not found!')); // Return an error
+        } 
+        const isMatch = bcryptjs.compareSync(password, validUser.password); // Compare the password with the hashed password
+        if (!isMatch) { // If the password does not match
+            return next(errorHandler(400, 'Invalid password!')); // Return an error
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' }); // Create a token    
+        const { password: pass, ...userDetails } = validUser._doc; // Destructure the password from the user object
+        res.cookie('token', token, { httpOnly: true }).status(200).json({userDetails}); // Set the token in a cookie
+
     } catch (error) {  
         next(error); // If there is an error, pass it to the error handling middleware
     }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js'; // Import the signup function from the controller
+import { signup, signin, testauth } from '../controllers/auth.controller.js'; // Import the signup function from the controller
 
 const router = express.Router(); // Create a new router
+
 router.post('/signup', signup); // Add a new route to the router
+router.post('/signin', signin); // Add a new route to the router
+router.get('/testauth', testauth); // Add a new route to the router
 
 export default router; // Export the router so it can be used in other files     
